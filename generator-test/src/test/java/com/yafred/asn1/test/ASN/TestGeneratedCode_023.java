3b2da52399e735ae0c9e6cc31764b5d2264ee8dd
Yet Another Fred <yafred@users.noreply.github.com>
2020-03-30 10:40:03 +0200

    More tests on ASN values

---
+++ generator-test/src/test/java/com/yafred/asn1/test/ASN/TestGeneratedCode_023.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright (C) 2020 Fred D7e (https://github.com/yafred)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+package com.yafred.asn1.test.ASN;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+
+import com.yafred.asn1.runtime.ASNValueReader;
+import com.yafred.asn1.runtime.ASNValueWriter;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import g_023.ChoiceWithEmptyComp;
+import g_023.EmptySequence;
+import g_023.EmptySet;
+
+
+
+public class TestGeneratedCode_023 {
+
+	@Test
+	public void test_1() throws Exception {
+		String asnValue = "{}";
+		
+		String expectedAsnValue = asnValue;
+
+				
+    	InputStream inputStream = new ByteArrayInputStream(asnValue.getBytes(StandardCharsets.UTF_8));
+    	ASNValueReader asnValueReader = new ASNValueReader(inputStream);
+    	
+    	EmptySet decodedPdu = EmptySet.readPdu(asnValueReader);
+    	
+		StringWriter stringWriter = new StringWriter(100);
+		ASNValueWriter asnValueWriter = new ASNValueWriter(new PrintWriter(stringWriter));
+		EmptySet.writePdu(decodedPdu, asnValueWriter);
+		
+		assertEquals(expectedAsnValue.replaceAll("\\s+",""), stringWriter.toString().replaceAll("\\s+",""));  		
+	}
+	
+	@Test
+	public void test_2() throws Exception {
+		String asnValue = "{}";
+		
+		String expectedAsnValue = asnValue;
+
+				
+    	InputStream inputStream = new ByteArrayInputStream(asnValue.getBytes(StandardCharsets.UTF_8));
+    	ASNValueReader asnValueReader = new ASNValueReader(inputStream);
+    	
+    	EmptySequence decodedPdu = EmptySequence.readPdu(asnValueReader);
+    	
+		StringWriter stringWriter = new StringWriter(100);
+		ASNValueWriter asnValueWriter = new ASNValueWriter(new PrintWriter(stringWriter));
+		EmptySequence.writePdu(decodedPdu, asnValueWriter);
+		
+		assertEquals(expectedAsnValue.replaceAll("\\s+",""), stringWriter.toString().replaceAll("\\s+",""));  		
+	}
+	
+	@Test
+	public void test_3() throws Exception {
+		String asnValue = "set : {}";
+		
+		String expectedAsnValue = asnValue;
+
+				
+    	InputStream inputStream = new ByteArrayInputStream(asnValue.getBytes(StandardCharsets.UTF_8));
+    	ASNValueReader asnValueReader = new ASNValueReader(inputStream);
+    	
+    	ChoiceWithEmptyComp decodedPdu = ChoiceWithEmptyComp.readPdu(asnValueReader);
+    	
+		StringWriter stringWriter = new StringWriter(100);
+		ASNValueWriter asnValueWriter = new ASNValueWriter(new PrintWriter(stringWriter));
+		ChoiceWithEmptyComp.writePdu(decodedPdu, asnValueWriter);
+		
+		assertEquals(expectedAsnValue.replaceAll("\\s+",""), stringWriter.toString().replaceAll("\\s+",""));  		
+	}
+}
