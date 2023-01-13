31bbf2671b287ad75cb7732a6dd28f8f82a05b05
YaFred <yafred@users.noreply.github.com>
2023-01-13 13:22:39 +0100

    Update coyright s

--- generator-test/src/test/java/com/yafred/asn1/test/ASN/TestGeneratedCode_015.java
+++ generator-test/src/test/java/com/yafred/asn1/test/ASN/TestGeneratedCode_015.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
 
- * Copyright (C) 2022 Fred D7e (https://github.com/yafred)
+ * Copyright (C) 2023 Fred D7e (https://github.com/yafred)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- model/src/main/java/com/yafred/asn1/model/type/BooleanType.java
+++ model/src/main/java/com/yafred/asn1/model/type/BooleanType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2022 Fred D7e (https://github.com/yafred)
+ * Copyright (C) 2023 Fred D7e (https://github.com/yafred)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
